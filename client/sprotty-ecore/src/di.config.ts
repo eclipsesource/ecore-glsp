37baa28ddcda99d45597b84fc03b1eb77f5daaab
Jonas Ebel <jonas.ebel@tum.de>
2019-11-08 09:45:15 +0100

    Fix #31 Add GLSP edit-label css to improve edit label styling  (#50)
    Signed-off-by: Jonas Ebel <jonas.ebel@tum.de>

--- client/sprotty-ecore/src/di.config.ts
+++ client/sprotty-ecore/src/di.config.ts
@@ -14,6 +14,7 @@
  *   SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ******************************************************************************/
 import "@glsp/sprotty-client/css/glsp-sprotty.css";
+import "sprotty/css/edit-label.css";
 
 import {
     boundsModule,
