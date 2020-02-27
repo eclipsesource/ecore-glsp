068a0355532de5ab0912405fae12b79ce5e29bba
Camille Letavernier <cletavernier@eclipsesource.com>
2020-02-27 16:46:28 +0100

    Update Copyright headers to match EMF.Cloud licenses
    - Define EPL2.0 OR MIT Licenses
    
    Signed-off-by: Camille Letavernier <cletavernier@eclipsesource.com>

--- server/org.eclipse.emfcloud.ecore.glsp/src/main/java/org/eclipse/emfcloud/ecore/glsp/EcoreServerLauncher.java
+++ server/org.eclipse.emfcloud.ecore.glsp/src/main/java/org/eclipse/emfcloud/ecore/glsp/EcoreServerLauncher.java
@@ -1,13 +1,13 @@
-/*******************************************************************************
- * Copyright (c) 2018 EclipseSource Services GmbH and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *   
- * Contributors:
- * 	Tobias Ortmayr - initial API and implementation
- ******************************************************************************/
+/********************************************************************************
+ * Copyright (c) 2019-2020 EclipseSource and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0, or the MIT License which is
+ * available at https://opensource.org/licenses/MIT.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR MIT
+ ********************************************************************************/
 package org.eclipse.emfcloud.ecore.glsp;
 
 import org.apache.log4j.BasicConfigurator;
